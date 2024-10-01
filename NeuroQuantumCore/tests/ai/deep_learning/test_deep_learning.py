66220221f5e63b2a4757cd5ef191009416d96df7
KOSASIH <kosasihg88@gmail.com>
2024-10-01 19:34:57 +0700

    Update test_deep_learning.py

--- NeuroQuantumCore/tests/ai/deep_learning/test_deep_learning.py
+++ NeuroQuantumCore/tests/ai/deep_learning/test_deep_learning.py
@@ -1,32 +1,20 @@
 import unittest
-from deep_learning import DeepLearningModel
+from deep_learning.deep_learning import DeepLearning
 
 class TestDeepLearning(unittest.TestCase):
-    def test_deep_learning_model(self):
-        dl = DeepLearningModel((784,), 10)
-        self.assertEqual(dl.input_shape, (784,))
-        self.assertEqual(dl.num_classes, 10)
+    def test_deep_learning(self):
+        deep_learning = DeepLearning()
+        self.assertIsNotNone(deep_learning.model)
 
     def test_train(self):
-        dl = DeepLearningModel((784,), 10)
-        X_train = np.random.rand(100, 784)
-        y_train = np.random.randint(0, 10, 100)
-        dl.train(X_train, y_train)
-        self.assertIsNotNone(dl.model)
-
-    def test_evaluate(self):
-        dl = DeepLearningModel((784,), 10)
-        X_test = np.random.rand(100, 784)
-        y_test = np.random.randint(0, 10, 100)
-        loss, accuracy = dl.evaluate(X_test, y_test)
-        self.assertGreaterEqual(accuracy, 0)
-        self.assertLessEqual(accuracy, 1)
+        deep_learning = DeepLearning()
+        deep_learning.train([[0, 0], [0, 1], [1, 0], [1, 1]], [[0], [1], [1], [0]])
+        self.assertIsNotNone(deep_learning.model)
 
     def test_predict(self):
-        dl = DeepLearningModel((784,), 10)
-        X = np.random.rand(1, 784)
-        prediction = dl.predict(X)
-        self.assertEqual(prediction.shape, (1, 10))
+        deep_learning = DeepLearning()
+        deep_learning.train([[0, 0], [0, 1], [1, 0], [1, 1]], [[0], [1], [1], [0]])
+        self.assertIsNotNone(deep_learning.predict([0, 0]))
 
 if __name__ == '__main__':
     unittest.main()
