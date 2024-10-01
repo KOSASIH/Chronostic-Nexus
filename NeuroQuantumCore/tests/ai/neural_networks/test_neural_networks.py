4d7758e368b280a9acefccb43e8e8f3d5dd716b0
KOSASIH <kosasihg88@gmail.com>
2024-10-01 19:34:03 +0700

    Update test_neural_networks.py

--- NeuroQuantumCore/tests/ai/neural_networks/test_neural_networks.py
+++ NeuroQuantumCore/tests/ai/neural_networks/test_neural_networks.py
@@ -1,32 +1,20 @@
 import unittest
-from neural_networks import NeuralNetwork
+from neural_networks.neural_networks import NeuralNetwork
 
 class TestNeuralNetworks(unittest.TestCase):
     def test_neural_network(self):
-        nn = NeuralNetwork((784,), 10)
-        self.assertEqual(nn.input_shape, (784,))
-        self.assertEqual(nn.num_classes, 10)
+        neural_network = NeuralNetwork (2, 2, 1)
+        self.assertIsNotNone(neural_network.weights)
 
     def test_train(self):
-        nn = NeuralNetwork((784,), 10)
-        X_train = np.random.rand(100, 784)
-        y_train = np.random.randint(0, 10, 100)
-        nn.train(X_train, y_train)
-        self.assertIsNotNone(nn.model)
-
-    def test_evaluate(self):
-        nn = NeuralNetwork((784,), 10)
-        X_test = np.random.rand(100, 784)
-        y_test = np.random.randint(0, 10, 100)
-        loss, accuracy = nn.evaluate(X_test, y_test)
-        self.assertGreaterEqual(accuracy, 0)
-        self.assertLessEqual(accuracy, 1)
+        neural_network = NeuralNetwork(2, 2, 1)
+        neural_network.train([[0, 0], [0, 1], [1, 0], [1, 1]], [[0], [1], [1], [0]])
+        self.assertIsNotNone(neural_network.weights)
 
     def test_predict(self):
-        nn = NeuralNetwork((784,), 10)
-        X = np.random.rand(1, 784)
-        prediction = nn.predict(X)
-        self.assertEqual(prediction.shape, (1, 10))
+        neural_network = NeuralNetwork(2, 2, 1)
+        neural_network.train([[0, 0], [0, 1], [1, 0], [1, 1]], [[0], [1], [1], [0]])
+        self.assertIsNotNone(neural_network.predict([0, 0]))
 
 if __name__ == '__main__':
     unittest.main()
