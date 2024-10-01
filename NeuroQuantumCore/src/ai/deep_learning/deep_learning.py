4b9f955b2b52c4b9020c7edeb2d90c1d9705b9e3
KOSASIH <kosasihg88@gmail.com>
2024-10-01 11:27:03 +0700

    Update deep_learning.py

--- NeuroQuantumCore/src/ai/deep_learning/deep_learning.py
+++ NeuroQuantumCore/src/ai/deep_learning/deep_learning.py
@@ -1,6 +1,9 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 class DeepLearningModel:
     def __init__(self, input_shape, num_classes):
@@ -9,22 +12,23 @@ class DeepLearningModel:
         self.model = self.build_model()
 
     def build_model(self):
-        model = tf.keras.models.Sequential([
-            Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape),
-            MaxPooling2D((2, 2)),
-            Conv2D(64, (3, 3), activation='relu'),
-            MaxPooling2D((2, 2)),
-            Conv2D(128, (3, 3), activation='relu'),
-            MaxPooling2D((2, 2)),
-            Flatten(),
-            Dense(128, activation='relu'),
-            Dense(self.num_classes, activation='softmax')
-        ])
-        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        model = Sequential()
+        model.add(Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Conv2D(128, (3, 3), activation='relu'))
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(self.num_classes, activation='softmax'))
+        model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
         return model
 
     def train(self, X_train, y_train, epochs=10, batch_size=32):
-        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+        early_stopping = EarlyStopping(monitor='val_loss', patience=5, min_delta=0.001)
+        model_checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True, mode='min')
+        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2, callbacks=[early_stopping, model_checkpoint])
 
     def evaluate(self, X_test, y_test):
         return self.model.evaluate(X_test, y_test)
@@ -39,20 +43,18 @@ class GenerativeAdversarialNetwork(DeepLearningModel):
         self.discriminator = self.build_discriminator()
 
     def build_generator(self):
-        model = tf.keras.models.Sequential([
-            Dense(128, activation='relu', input_shape=(100,)),
-            Dense(128, activation='relu'),
-            Dense(self.input_shape[1], activation='tanh')
-        ])
+        model = Sequential()
+        model.add(Dense(128, activation='relu', input_shape=(100,)))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(self.input_shape[1], activation='tanh'))
         return model
 
     def build_discriminator(self):
-        model = tf.keras.models.Sequential([
-            Dense(128, activation='relu', input_shape=self.input_shape),
-            Dense(128, activation='relu'),
-            Dense(1, activation='sigmoid ')
-        ])
-        model.compile(optimizer='adam', loss='binary_crossentropy')
+        model = Sequential()
+        model.add(Dense(128, activation='relu', input_shape=self.input_shape))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy')
         return model
 
     def train(self, X_train, y_train, epochs=10, batch_size=32):
@@ -63,3 +65,32 @@ class GenerativeAdversarialNetwork(DeepLearningModel):
                 d_loss_real = self.discriminator.train_on_batch(X_train[i:i+batch_size], np.ones((batch_size, 1)))
                 d_loss_fake = self.discriminator.train_on_batch(generated_images, np.zeros((batch_size, 1)))
                 g_loss = self.generator.train_on_batch(noise, np.ones((batch_size, 1)))
+
+class VariationalAutoencoder(DeepLearningModel):
+    def __init__(self, input_shape, num_classes):
+        super().__init__(input_shape, num_classes)
+        self.encoder = self.build_encoder()
+        self.decoder = self.build_decoder()
+
+    def build_encoder(self):
+        model = Sequential()
+        model.add(Dense(128, activation='relu', input_shape=self.input_shape))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(self.input_shape[1], activation='sigmoid'))
+        return model
+
+    def build_decoder(self):
+        model = Sequential()
+        model.add(Dense(128, activation='relu', input_shape=(self.input_shape[1],)))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(self.input_shape[1], activation='sigmoid'))
+        return model
+
+    def train(self, X_train, y_train, epochs=10, batch_size=32):
+        for epoch in range(epochs):
+            for i in range(len(X_train)):
+                encoded_data = self.encoder.predict(X_train[i:i+batch_size])
+                decoded_data = self.decoder.predict(encoded_data)
+                loss = np.mean((decoded_data - X_train[i:i+batch_size]) ** 2)
+                self.encoder.train_on_batch(X_train[i:i+batch_size], encoded_data)
+                self.decoder.train_on_batch (encoded_data, decoded_data)
