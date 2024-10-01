e103648a1ead2b8ada07e7d0b668c4dd863b7ed2
KOSASIH <kosasihg88@gmail.com>
2024-10-01 11:41:39 +0700

    Create cryptography.py

---
+++ ChronoChain/src/cryptography/cryptography.py
@@ -0,0 +1,114 @@
+import hashlib
+import random
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+class Cryptography:
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+        self.public_key = self.private_key.public_key()
+
+    def generate_keys(self):
+        return self.private_key, self.public_key
+
+    def sign_transaction(self, transaction):
+        signature = self.private_key.sign(
+            transaction.hash.encode(),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return signature
+
+    def verify_transaction(self, transaction, signature):
+        try:
+            self.public_key.verify(
+                signature,
+                transaction.hash.encode(),
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+            return True
+        except:
+            return False
+
+class HashFunction:
+    def __init__(self):
+        self.hash_function = hashlib.sha256()
+
+    def hash(self, data):
+        return self.hash_function.update(data.encode()).hexdigest()
+
+class DigitalSignature:
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+        self.public_key = self.private_key.public_key()
+
+    def sign(self, data):
+        signature = self.private_key.sign(
+            data.encode(),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return signature
+
+    def verify(self, data, signature):
+        try:
+            self.public_key.verify(
+                signature,
+                data.encode(),
+                padding.PSS(
+                    mgf=padding.MGF1(hashes.SHA256()),
+                    salt_length=padding.PSS.MAX_LENGTH
+                ),
+                hashes.SHA256()
+            )
+            return True
+        except:
+            return False
+
+class Encryption:
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+        )
+        self.public_key = self.private_key.public_key()
+
+    def encrypt(self, data):
+        encrypted_data = self.public_key.encrypt(
+            data.encode(),
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return encrypted_data
+
+    def decrypt(self, encrypted_data):
+        decrypted_data = self.private_key.decrypt(
+            encrypted_data,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return decrypted_data.decode()
